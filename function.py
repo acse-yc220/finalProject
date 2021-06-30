f09adfa01835534ae140dfc2a30b99d38bc2bd80
acse-yc220 <72437026+acse-yc220@users.noreply.github.com>
2021-06-30 10:12:19 +0100

    Add files via upload

--- function.py
+++ function.py
@@ -1,3 +1,4 @@
+from os import remove
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,27 +7,19 @@ import matplotlib.cm as cm
 import xlrd
 from matplotlib.colors import LogNorm
 from sklearn.preprocessing import StandardScaler
+import hdbscan
 
-def plot_input(fig,axarr,i,j,value,color,label):
+def plot_input(fig,axarr,i,j,value,color,label,vmin=np.nan,vmax=np.nan):
+    if np.isnan(vmin):
+        vmin=np.nanmin(value)
+    if np.isnan(vmax):
+        vmax=np.nanmax(value)
     a=axarr[i][j].imshow(value, extent=(np.amin(data['x']), np.amax(data['x']), 
-         np.amax(data['z']),np.amin(data['z'])),
+         np.amax(data['z']),np.amin(data['z'])), vmin=vmin, vmax=vmax,
         cmap=color)
     cb = fig.colorbar(a,ax=axarr[i][j])
     cb.set_label(label)
-    #plt.xlim(0，20)
-   # h = plt.contourf(value,cmap = cm.viridis_r)
-  #  c = plt.colorbar(h) 
 
-# def show_batch(dataset, nr, nc):
-#     fig, axarr = plt.subplots(nr, nc, figsize=(15, 15)) # nr is the number of rows
-#                                                         # nc is the number of columns
-#     plot_input(fig,axarr,0,0,dataset['vp'],cm.viridis_r,'Vp(m/s)')
-#     plot_input(fig,axarr,0,1,dataset['vs'],cm.viridis_r,'Vs(m/s)')
-#     plot_input(fig,axarr,1,0,dataset['dn'],cm.magma_r,'Density(kg/m^3)')
-#     plot_input(fig,axarr,1,1,dataset['vpvs'],cm.magma_r,'Vp/Vs')
-#     plot_input(fig,axarr,2,0,dataset['qp'],cm.magma_r,'Qp^(-1)')
-#     plot_input(fig,axarr,2,1,dataset['qs'],cm.magma_r,'Qs^(-1)')
-#     plt.show()
 
 def show_data(dataset,nr,nc):
     fig, axarr = plt.subplots(nr, nc, figsize=(15, 15)) # nr is the number of rows
@@ -35,8 +28,8 @@ def show_data(dataset,nr,nc):
     plot_input(fig,axarr,0,1,dataset[1],cm.viridis_r,'Vs(m/s)')
     plot_input(fig,axarr,1,0,dataset[2],cm.magma_r,'Density(kg/m^3)')
     plot_input(fig,axarr,1,1,dataset[3],cm.magma_r,'Vp/Vs')
-    plot_input(fig,axarr,2,0,dataset[4],cm.magma_r,'Qp^(-1)')
-    plot_input(fig,axarr,2,1,dataset[5],cm.magma_r,'Qs^(-1)')
+    plot_input(fig,axarr,2,0,dataset[4],cm.magma_r,'Qp')
+    plot_input(fig,axarr,2,1,dataset[5],cm.magma_r,'Qs')
     plt.show()
 
 def loadData(dataset):
@@ -45,17 +38,50 @@ def loadData(dataset):
         data.append(dataset[dataset.files[i]])
     return data
 
+
+def removenans(dataset):
+# Function that removes points with nan values from array
+# Store the indices of the removed points so that they can be added back later (after clustering)
+# Option 1:
+# - add a dimension to the array where you store the initial indices
+# - remove elements with nans and place removed elements in new array
+# - perorm clustering
+# - write separate function that restores removed elements
+# inputs = full dataset
+# outputs = trimmed dataset and array of removed elements
+    new_data = []
+    for i in range(len(dataset['z'])):
+        for j in range(len(dataset['x'])):
+            for k in range(len(dataset.files)-2):
+                new_data.append(dataset[dataset.files[k]][i][j])
+            new_data.append(dataset['x'][j])
+            new_data.append(dataset['z'][i])
+    new_data = np.array(new_data).reshape(-1,8)
+    nan_list = np.isnan(new_data).any(axis=1)
+    data_noNan = new_data[~nan_list,:]
+    remove_list = new_data[nan_list,:]
+    return new_data,data_noNan,remove_list
+
+#def restorenans(dataset,nanarray):
+# - paste removed elements to back of array
+# - sort elements by original index 
+# inputs = trimmed dataset and array of removed elements
+# outputs = restored dataset
+
+
 def preprocessing(dataset):
     data_preprocessing = []
-    for i in range(len(dataset)):
-        data_inf = np.isinf(dataset[i])
-        dataset[i][data_inf] = 0
-        data_nan = np.isnan(dataset[i])
-        dataset[i][data_nan] = 0
-        scaler = StandardScaler()
-        scaler.fit(dataset[i])
-        data_normalized = scaler.transform(dataset[i])
-        data_preprocessing.append(data_normalized)
+    for i in range(dataset.shape[1]-2):
+        mean=np.nanmean(dataset[:,i])
+        var=np.nanstd(dataset[:,i])
+        y=(dataset[:,i]-mean)/var
+        data_preprocessing.append(y)
+    data_preprocessing.append(dataset[:,-2])
+    data_preprocessing.append(dataset[:,-1])
+    # data_preprocessing[1]=np.clip(data_preprocessing[1],None,2)
+    # data_preprocessing[3]=np.clip(data_preprocessing[3],None,2)
+    data_preprocessing = np.array(data_preprocessing).T
+    print(data_preprocessing.shape)
     return data_preprocessing
     # for i in range(len(dataset.files)-2):
     #     # print(np.isnan(dataset[dataset.files[i]]))
@@ -71,9 +97,9 @@ def preprocessing(dataset):
     # return data_preprocessing
 
 def showDistribution(dataset):
-    for i in range(len(dataset)):
+    for i in range(dataset.shape[1]):
         ax = plt.subplot(3,2,i+1)
-        data_distribution = dataset[i].reshape(1,-1)
+        data_distribution = dataset[:,i]
         value = np.isfinite(data_distribution)
         ax.set_title(data.files[i])
         plt.hist(data_distribution[value])
@@ -81,46 +107,36 @@ def showDistribution(dataset):
     plt.show()
 
 def crossplot(dataset):
-    fig, ax = plt.subplots(3,2,figsize=(15, 20),subplot_kw={"projection": "3d"})
-    for i in range(len(dataset)):
-        xx, yy = np.meshgrid(data['x'], data['z'] )
-        data_inf = np.isinf(dataset[i])
-        dataset[i][data_inf] = 0
-        data_nan = np.isnan(dataset[i])
-        dataset[i][data_nan] = 0
-        zz = dataset[i]
-        row_index = i//2
-        col_index = i%2
-        surf = ax[row_index][col_index].plot_surface(xx, yy, zz, cmap='viridis', edgecolor='none',
-                        linewidth=0, antialiased=True,  rstride=1, cstride=1,)
-        ax[row_index][col_index].set_xlabel(r'x')
-        ax[row_index][col_index].set_ylabel(r'z')
-        ax[row_index][col_index].set_zlabel(data.files[i])
-        cb = fig.colorbar(surf, shrink=0.5, aspect=5,ax=ax[row_index][col_index])
-        cb.set_label(data.files[i])
+    for i in range(5):
+        ax = plt.subplot(3,2,i+1)
+        plt.scatter(dataset[:,0], dataset[:,i+1])
+        ax.set_xlabel(data.files[0])
+        ax.set_ylabel(data.files[i+1])
     plt.tight_layout()
     plt.show()
-
-def dataForm(dataset):
-     n = len(dataset)
-     row_num = dataset[0].shape[0]
-     col_num = dataset[0].shape[1]
-     data = np.zeros((row_num*col_num,n))
-     for i in range(row_num):
-         for j in range(col_num):
-             for dim in range(n):
-                 data[i*col_num+j,dim] = dataset[dim][i,j]
-     return data
+# def dataForm(dataset):
+#      n = len(dataset)
+#      row_num = dataset[0].shape[0]
+#      col_num = dataset[0].shape[1]
+#      data = np.zeros((row_num*col_num,n))
+#      for i in range(row_num):
+#          for j in range(col_num):
+#              for dim in range(n):
+#                  data[i*col_num+j,dim] = dataset[dim][i,j]
+#      return data
 
 def distEclud(vecA, vecB):
      return np.sqrt(np.sum(np.power(vecA - vecB, 2))) 
 
 def randCent(dataset, k):
      n = dataset.shape[1]
-     centroids = np.zeros((k,n))   
-     for i in range(k):
+     centroids = np.mat(np.zeros((k,n)))   
+     for i in range(n):
+         data_min = np.min(dataset[:,i])
+         data_max = np.max(dataset[:,i])
+         data_range = np.float(data_max-data_min)
          index = np.random.randint(dataset[0].size)
-         centroids[i,:] = dataset[index,:]
+         centroids[:,i] = data_min + data_range * np.random.rand(k, 1)
      return centroids
 
 def kMeans(dataset, k, distMeans =distEclud, createCent = randCent):
@@ -141,13 +157,17 @@ def kMeans(dataset, k, distMeans =distEclud, createCent = randCent):
                 if clusterAssment[i,0] != minIndex: 
                     clusterChanged = True
                 clusterAssment[i,:] = minIndex,minDist**2   
-         print(clusterAssment)
          clusterAssment = np.array(clusterAssment)
          for cent in range(k):  
              ptsInClust = dataset[np.nonzero(clusterAssment[:,0] == cent)[0]]  
              centroids[cent,:] = np.mean(ptsInClust, axis = 0)  
      return centroids, clusterAssment
 
+
+def HDBSCAN(dataset):
+    clusterer = hdbscan.HDBSCAN(min_cluster_size=5, gen_min_span_tree=True)
+    clusterer.fit(dataset)
+    
 
 
 # def showCluster(dataSet, k, centroids, clusterAssment):
@@ -171,7 +191,30 @@ def kMeans(dataset, k, distMeans =distEclud, createCent = randCent):
  
 # 	plt.show()
 
-data = np.load('C:/Users/12928/Desktop/acse9/SyntheticDatasets/Model5b/output_fields.npz')
+data = np.load('C:/Users/12928/Desktop/SyntheticDatasets/Model5b/output_fields_smooth.npz')
+#print(data[data.files[0]].shape)
+#data_list = loadData(data)
+# data_inf = np.isinf(data_list[3])
+# data_list[3][data_inf] = np.nan
+
+# data_list[0]= 1./data_list[0]
+# data_list[1]= 1./data_list[1]
+# data_inf = np.isinf(data_list[1])
+# data_list[1][data_inf] = np.nan
+
+
+# show_data(data_list,3,2)
+
+#data_list[3]=np.clip(data_list[3],None,2)
+# new_data,data_noNan,remove_list = removenans(data)
+#showDistribution(new_data)
+# crossplot(new_data[:,0],new_data[:,1])
+
+
+# data_preprocessing = preprocessing(new_data)
+# showDistribution(data_preprocessing)
+
+
 #data1 = loadData(data)
 # crossplot(data1)
 #data2 = preprocessing(data1)
@@ -198,8 +241,15 @@ data = np.load('C:/Users/12928/Desktop/acse9/SyntheticDatasets/Model5b/output_fi
 # print(a)
 # plt.show()  
 
-#centroids,clusterAssment = kMeans(data_new,4)
 
+# centroids,clusterAssment = kMeans(data_noNan,2)
+# print(clusterAssment)
+# col = ['HotPink','Aqua','Chartreuse','yellow','red','blue','green','grey','orange'] 
+# for i in range(data_noNan.shape[0]):
+#     plt.scatter(data_noNan[i][0],data_noNan[i][1],color=col[np.int(clusterAssment[i][0])-1])
+
+# #plt.scatter(centroids[i][0],centroids[i][1],linewidth=3,s=300,marker='+',color='black')
+# plt.show()
 # print(myCentroids)
 # print(clusterAssment)
 # showCluster(data_new, 4, centroids, clusterAssment)
